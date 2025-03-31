f585220ef62df850d30456efa39ed7e38d8d2095
Justine Barcelon <justinebarcelon18@gmail.com>
2025-03-31 11:16:29 +0800

    basta goods

---
+++ noVNC/start_websockify.bat
@@ -0,0 +1,4 @@
+@REM @echo off
+@REM cd /d C:\xampp\htdocs\NexusControll\noVNC
+@REM start /min python -m websockify --verbose 6080 192.168.1.19:5901 --web C:\xampp\htdocs\NexusControll\noVNC
+@REM exit
